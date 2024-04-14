a5948963260c159b4f3e2fe6db5576eadc2f3071
ertugrulcakici <ertugrul.cakicii@gmail.com>
2024-04-15 00:59:11 +0300

    Refactor asm-module.asm to add new functions and optimize existing ones

--- a4/asm-module.asm
+++ a4/asm-module.asm
@@ -64,3 +64,148 @@ hex2int:
 .done:
     pop ebp                 ; restore old base pointer
     ret                     ; Return with the result in EAX
+
+; ==============================================================================
+
+; void clean_no_prime(int *tp_array, int t_N)
+; Set to zero all numbers in int array which are not prime.
+clean_no_prime:
+    push rbp
+    mov rbp, rsp
+    
+    mov r8, rdi ; r8 = tp_array
+    mov r9d, esi ; r9d = t_N
+    xor r10, r10 ; r10 = i = 0
+    
+.loop:
+    cmp r10d, r9d
+    je .end
+    
+    mov eax, [r8 + 4*r10] ; eax = tp_array[i]
+    cmp eax, 2
+    je .is_prime
+    
+    ; Check if number is prime
+    mov ecx, 2
+.is_prime_loop:
+    cmp ecx, eax
+    je .is_prime
+    test eax, ecx
+    jz .set_to_zero
+    inc ecx
+    jmp .is_prime_loop
+    
+.set_to_zero:
+    mov dword [r8 + 4*r10], 0
+    
+.is_prime:
+    inc r10
+    jmp .loop
+    
+.end:
+    pop rbp
+    ret
+
+; int modulo_0(int *tp_array, int t_N, int t_M)
+; How many numbers in array has modulo of M equal to zero?
+modulo_0:
+    push rbp
+    mov rbp, rsp
+    
+    mov r8, rdi ; r8 = tp_array
+    mov r9d, esi ; r9d = t_N
+    mov r10d, edx ; r10d = t_M
+    xor eax, eax ; eax = count = 0
+    xor r11, r11 ; r11 = i = 0
+    
+.loop:
+    cmp r11d, r9d
+    je .end
+    
+    mov ecx, [r8 + 4*r11] ; ecx = tp_array[i]
+    xor edx, edx
+    div r10d
+    cmp edx, 0
+    je .increment
+    
+.continue:
+    inc r11
+    jmp .loop
+    
+.increment:
+    inc eax
+    jmp .continue
+    
+.end:
+    pop rbp
+    ret
+
+; long factorial(long t_num)
+; Compute factorial of long. If result overflow, return 0.
+factorial:
+    push rbp
+    mov rbp, rsp
+    
+    mov rax, rdi ; rax = t_num
+    cmp rax, 0
+    je .zero
+    
+    mov rcx, 1 ; rcx = 1
+    
+.loop:
+    cmp rax, rcx
+    jle .end
+    
+    ; Multiply rax and rcx, check for overflow
+    mul rcx
+    jo .overflow
+    
+    inc rcx
+    jmp .loop
+    
+.end:
+    pop rbp
+    ret
+    
+.zero:
+    mov rax, 1
+    pop rbp
+    ret
+    
+.overflow:
+    xor rax, rax
+    dec rax ; rax = -1
+    pop rbp
+    ret
+
+; int hex2int(const char *hexStr)
+; Convert hex-string into number.
+hex2int:
+    push rbp
+    mov rbp, rsp
+    
+    mov rdx, rdi ; rdx = hexStr
+    xor eax, eax ; eax = result = 0
+    
+.loop:
+    movzx ecx, byte [rdx] ; ecx = current character
+    test cl, cl
+    jz .end
+    
+    ; Convert ASCII to hex value
+    sub ecx, '0'
+    cmp ecx, 9
+    jbe .is_digit
+    sub ecx, 7
+    sub ecx, 32
+    and ecx, 0xF
+    
+.is_digit:
+    shl eax, 4 ; Shift result left by 4 bits
+    or eax, ecx ; Add current digit to result
+    inc rdx ; Move to next character
+    jmp .loop
+    
+.end:
+    pop rbp
+    ret
