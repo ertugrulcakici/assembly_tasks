2993bbf29c66e7643b7115d71df2088efa8f004b
ertugrulcakici <ertugrul.cakicii@gmail.com>
2024-04-15 01:04:25 +0300

    Add new functions and optimize existing ones in asm-module.asm

--- a4/asm-module.asm
+++ a4/asm-module.asm
@@ -3,6 +3,11 @@ bits 64
 section .text
 global hex2int
 global factorial
+global clean_no_prime
+global modulo_0
+global power
+global up_low
+
 
 factorial:
     push rbp                
@@ -65,10 +70,6 @@ hex2int:
     pop ebp                 ; restore old base pointer
     ret                     ; Return with the result in EAX
 
-; ==============================================================================
-
-; void clean_no_prime(int *tp_array, int t_N)
-; Set to zero all numbers in int array which are not prime.
 clean_no_prime:
     push rbp
     mov rbp, rsp
@@ -106,8 +107,7 @@ clean_no_prime:
     pop rbp
     ret
 
-; int modulo_0(int *tp_array, int t_N, int t_M)
-; How many numbers in array has modulo of M equal to zero?
+
 modulo_0:
     push rbp
     mov rbp, rsp
@@ -140,70 +140,88 @@ modulo_0:
     pop rbp
     ret
 
-; long factorial(long t_num)
-; Compute factorial of long. If result overflow, return 0.
-factorial:
+
+power:
     push rbp
     mov rbp, rsp
     
-    mov rax, rdi ; rax = t_num
-    cmp rax, 0
-    je .zero
-    
-    mov rcx, 1 ; rcx = 1
+    mov r8, rdi ; r8 = tp_array
+    mov r9d, esi ; r9d = t_N
+    mov r10d, edx ; r10d = t_X
+    xor r11, r11 ; r11 = i = 0
     
 .loop:
-    cmp rax, rcx
-    jle .end
+    cmp r11d, r9d
+    je .end
     
-    ; Multiply rax and rcx, check for overflow
-    mul rcx
+    mov eax, [r8 + 4*r11] ; eax = tp_array[i]
+    mov ecx, r10d ; ecx = t_X
+    
+    ; Compute power
+    xor edx, edx
+.power_loop:
+    test ecx, 1
+    jz .next
+    imul eax, [r8 + 4*r11]
     jo .overflow
     
-    inc rcx
+.next:
+    shl [r8 + 4*r11], 1
+    jo .overflow
+    shr ecx, 1
+    jnz .power_loop
+    
+    mov [r8 + 4*r11], eax
+    inc r11
+    jmp .loop
+    
+.overflow:
+    mov dword [r8 + 4*r11], 0
+    inc r11
     jmp .loop
     
 .end:
     pop rbp
     ret
-    
-.zero:
-    mov rax, 1
-    pop rbp
-    ret
-    
-.overflow:
-    xor rax, rax
-    dec rax ; rax = -1
-    pop rbp
-    ret
 
-; int hex2int(const char *hexStr)
-; Convert hex-string into number.
-hex2int:
+; void up_low(char *tp_str, int t_up_low)
+; Convert string to upper or lower case optionally.
+up_low:
     push rbp
     mov rbp, rsp
     
-    mov rdx, rdi ; rdx = hexStr
-    xor eax, eax ; eax = result = 0
+    mov r8, rdi ; r8 = tp_str
+    mov r9d, esi ; r9d = t_up_low
+    xor r10, r10 ; r10 = i = 0
     
 .loop:
-    movzx ecx, byte [rdx] ; ecx = current character
+    mov cl, [r8 + r10] ; cl = current character
     test cl, cl
     jz .end
     
-    ; Convert ASCII to hex value
-    sub ecx, '0'
-    cmp ecx, 9
-    jbe .is_digit
-    sub ecx, 7
-    sub ecx, 32
-    and ecx, 0xF
+    cmp r9d, 0 ; t_up_low == 0
+    je .to_lower
     
-.is_digit:
-    shl eax, 4 ; Shift result left by 4 bits
-    or eax, ecx ; Add current digit to result
-    inc rdx ; Move to next character
+.to_upper:
+    cmp cl, 'a'
+    jb .next
+    cmp cl, 'z'
+    ja .next
+    sub cl, 32
+    jmp .update
+    
+.to_lower:
+    cmp cl, 'A'
+    jb .next
+    cmp cl, 'Z'
+    ja .next
+    add cl, 32
+    
+.update:
+    mov [r8 + r10], cl
+    
+.next:
+    inc r10
     jmp .loop
     
 .end:
