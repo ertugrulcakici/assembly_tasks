787fc357ae761952efd393cd61f5bfdca07f93db
ertugrulcakici <ertugrul.cakicii@gmail.com>
2024-04-15 12:40:35 +0300

    Refactor C-main.c to remove commented out code and unused functions

--- a4/c-main.c
+++ a4/c-main.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void power(int *tp_array, int t_N, int t_X);   // Compute sequence power of X to N. If result overflow, set result to 0.
-extern void up_low(char *tp_str, int t_up_low);       // Convert string to upper or lower case optionally.
-extern long factorial(long t_num);                    // Compute factorial of long. If result overflow, return 0.
-extern void clean_no_prime(int *tp_array, int t_N);   // Set to zero all numbers in int array which are not prime.
-extern int hex2int(const char *hexStr);               // Convert hex-string into number. // tp_str[] = "CAFE";
+// extern void power(int *tp_array, int t_N, int t_X);   // Compute sequence power of X to N. If result overflow, set result to 0.
+// extern void up_low(char *tp_str, int t_up_low);       // Convert string to upper or lower case optionally.
+extern long factorial(long t_num);                  // Compute factorial of long. If result overflow, return 0.
+extern void clean_no_prime(int *tp_array, int t_N); // Set to zero all numbers in int array which are not prime.
+// extern int hex2int(const char *hexStr);               // Convert hex-string into number. // tp_str[] = "CAFE";
 extern int modulo_0(int *tp_array, int t_N, int t_M); // How many numbers in array has modulo of M equal to zero?
 
-void for_power();
-void for_up_low();
+// void for_power();
+// void for_up_low();
 void for_factorial();
 void for_clean_no_prime();
-void for_hex2int();
+// void for_hex2int();
 void for_modulo_0();
 
 int main(int argc, char const *argv[])
 {
-    printf("Assignment 4\nFor power\n");
+    printf("Assignment 4\n");
+    // printf("For power\n");
     // for_power();
-    printf("\nFor up_low\n");
-    for_up_low();
+    // printf("\nFor up_low\n");
+    // for_up_low();
     printf("\nFor factorial\n");
     for_factorial();
     printf("\nFor clean_no_prime\n");
@@ -31,26 +32,26 @@ int main(int argc, char const *argv[])
     for_modulo_0();
 }
 
-void for_power()
-{
-    int arr[] = {1, 2, 3, 4, 5};
-    int N = sizeof(arr) / sizeof(arr[0]);
-    int X = 2;
-    power(arr, N, X);
+// void for_power()
+// {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int N = sizeof(arr) / sizeof(arr[0]);
+//     int X = 2;
+//     power(arr, N, X);
 
-    for (int i = 0; i < N; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+//     for (int i = 0; i < N; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+// }
 
-void for_up_low()
-{
-    char str[] = "Hello World";
-    up_low(str, 0);
-    printf("String in lower case: %s\n", str);
-}
+// void for_up_low()
+// {
+//     char str[] = "Hello World";
+//     up_low(str, 0);
+//     printf("String in lower case: %s\n", str);
+// }
 
 void for_factorial()
 {
@@ -80,13 +81,13 @@ void for_clean_no_prime()
     printf("\n");
 }
 
-void for_hex2int()
-{
-    const char *hexString = "CAFE";
-    int result = hex2int(hexString);
+// void for_hex2int()
+// {
+//     const char *hexString = "CAFE";
+//     int result = hex2int(hexString);
 
-    printf("The hexadecimal string %s is %d in decimal.\n", hexString, result);
-}
+//     printf("The hexadecimal string %s is %d in decimal.\n", hexString, result);
+// }
 
 void for_modulo_0()
 {
